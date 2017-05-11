94333ab2568313cea237606c559ae3ded6a0312c
wuguofeng <wu873191083@163.com>
2017-05-11 20:08:23 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/administrator/weatherforecast/Database/City.java
@@ -0,0 +1,49 @@
+package com.example.administrator.weatherforecast.Database;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wu873191083 on 2017/5/11.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+
+}
